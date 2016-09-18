9f5db97078b45b1328757c9991a181b32bda0d46
Marc <phyrenight@gmail.com>
2016-09-17 23:25:25 -0400

    info for each streamer is append to html

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,40 @@ function getStreamers(stream){
 }
 function i(){
 	for(i in streamers){
-	  var n = getStreamers(streamers[i])
-	  console.log(n)
+	  var data = getStreamers(streamers[i])
+      if(data.stream == null){
+      	streamers[i].stream = null;
+      }else{
+      	streamers[i].stream = "active";
+      	streamers[i].game = data.stream.game;
+      	streamers[i].details = data.stream.channel.status;
+      	streamers[i].logo = data.stream.channel.logo;
+      	streamers[i].url = data.stream.channel.url;
+      }
+      console.log(streamers[0].url)
     }
+    loadAll()
+}
+function loadAll(){
+	var height = 20;
+	var width = 20;
+	$streamers = $("#streamers");
+	for(i in streamers){
+		var streamersHtml = 0;
+		if(streamers[i].stream == null){
+			 streamersHtml = "<li><a href='https://www.twitch.tv/"+
+			 streamers[i].name+"'><img src='#' height="+height+" width="+
+			 width+ "alt='"+streamers[i].name+"' title='"+
+			 streamers[i].name+"'></a><p>"+streamers[i].name+
+			 " status: <span class='offline'>offline</span></p></li>";
+	   }else{
+	   	streamersHtml = "<li><a href='"+streamers[i].url+"'><img src='"+
+	   	streamers[i].logo+"' height="+height+"width="+width+"alt='"+
+	   	streamers[i].name+"' title='"+streamers[i].name+"'></a><p>"
+	   	+streamers[i].name+" status: <span class='active'>"+
+	   	     streamers[i].stream+"</span></p><p>game: "+streamers[i].game+
+	   	     " details: "+streamers[i].details+"</p></li>"
+	   }
+       $streamers.append(streamersHtml)
+	}
 }
